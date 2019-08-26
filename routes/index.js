b8b2914c3d30e58fe29fba44d3b5531e114cab7e
asthriona <im@asthriona.com>
2019-08-26 23:16:43 +0200

    classic release

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-res.render('index', { title: 'The Wall' });
+//router.get('/', function(req, res, next) {
+//res.render('index', { title: 'The Wall' });
+//});
+//router.get('/classic', function(req, res, next) {
+//  res.render('classic', { title: 'The Wall - Classic', layout: 'classic' })
+//});
+//router.get('/classic/no',function(req,res,next) {
+//  res.render('no', {title: 'The Wall - Classic - No.', layout: 'classic'})
+//});
+
+//classic
+router.get('/',function(req,res,next) {
+  res.render('classic', {title: 'The Wall - Classic - No.', layout: 'classic'})
+});
+router.get('/no',function(req,res,next) {
+  res.render('no', {title: 'The Wall - Classic - No.', layout: 'classic'})
 });
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'The Wall' });
   });
-router.get('/classic', function(req, res, next) {
-  res.render('classic', { title: 'The Wall - Classic', layout: 'classic' })
-});
-router.get('/classic/no',function(req,res,next) {
-  res.render('no', {title: 'The Wall - Classic - No.', layout: 'classic'})
-});
 
 module.exports = router;
